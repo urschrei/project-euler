3dc140f7ea847574e30e716e4c354640f8318515
Stephan Hügel <urschrei@gmail.com>
2011-06-15 20:23:29 +0100

    Use xrange, docstring consistency

--- problem_5.py
+++ problem_5.py
@@ -10,13 +10,13 @@ from fractions import gcd
 
 def lcm(largest):
     """
-    Use reduce() to perform successive LCM operations
+    use reduce() to perform successive LCM operations
     on the products of the previous result and the next item in the range.
     We can use reduce() because the chosen LCM algorithm is associative:
     http://en.wikipedia.org/wiki/Associativity
     http://en.wikipedia.org/wiki/Least_common_multiple#Lattice-theoretic
     """
-    return reduce(lambda x, y: (x * y) / gcd(x, y), range(1, largest))
+    return reduce(lambda x, y: (x * y) / gcd(x, y), xrange(1, largest))
 
 
 print lcm(20)
